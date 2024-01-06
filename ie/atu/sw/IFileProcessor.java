2caa329db1bcfa323b5095d8b0abd3cbc7e60252
olly <ollythemadbastard@gmail.com>
2024-01-06 11:31:31 +0000

    added the rest of the javadoc commands

--- ie/atu/sw/IFileProcessor.java
+++ ie/atu/sw/IFileProcessor.java
@@ -1,5 +1,25 @@
 package ie.atu.sw;
+
+/**
+ * This interface defines the methods that a file processor should implement.
+ * A file processor is responsible for parsing a file and processing a line of text.
+ */
 public interface IFileProcessor {
+
+    /**
+     * Parses a file and returns a double value.
+     *
+     * @param file The file to parse.
+     * @return A double value resulting from the file parsing.
+     * @throws Exception If an error occurs while parsing the file.
+     */
     double parseFile(String file) throws Exception;
+
+    /**
+     * Processes a line of text and returns a double value.
+     *
+     * @param text The line of text to process.
+     * @return A double value resulting from the text processing.
+     */
     double process(String text);
 }
--- ie/atu/sw/LexiconLoader.java
+++ ie/atu/sw/LexiconLoader.java
@@ -5,31 +5,39 @@ import java.nio.file.Paths;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.Executors;
 
-public class LexiconLoader  {
+/**
+ * This class is responsible for loading and parsing a lexicon file.
+ * The lexicon file is expected to be a txt file where each line contains a word and its sentiment value.
+ */
+public class LexiconLoader {
 
+    /**
+     * A map that stores the words of the lexicon and their sentiment values.
+     */
+    ConcurrentSkipListMap<String, Double> lexicon = new ConcurrentSkipListMap<>();
 
-
-
-ConcurrentSkipListMap<String, Double> lexicon = new ConcurrentSkipListMap<>();
- 
+    /**
+     * Parses a lexicon file and returns a map of words and their sentiment values.
+     *
+     * @param file The lexicon file to parse.
+     * @return A map of words and their sentiment values.
+     * @throws Exception If an error occurs while parsing the lexicon file.
+     */
     public ConcurrentSkipListMap<String, Double> parseLexicon(String file) throws Exception {
-
-        
-        
         try (var pool = Executors.newVirtualThreadPerTaskExecutor()){
-			Files.lines(Paths.get(file)).forEach(text -> pool.execute(() -> process(text)));
-		}
-		return lexicon;
-	}
-	
-	public void process(String text) {
+            Files.lines(Paths.get(file)).forEach(text -> pool.execute(() -> process(text)));
+        }
+        return lexicon;
+    }
+
+    /**
+     * Processes a line of text from the lexicon file.
+     * The line is expected to contain a word and its sentiment value, separated by a comma.
+     *
+     * @param text The line of text to process.
+     */
+    public void process(String text) {
         String [] parts = text.split(",");
         lexicon.put(parts[0], Double.parseDouble(parts[1]));
-
-	}
-	
-
-        
-        
-    
+    }
 }
