9368649a0c8153f51816dfe9d4ba266ffa9e7a29
olly <ollythemadbastard@gmail.com>
2024-01-06 13:18:22 +0000

    readme changes

--- ie/atu/sw/Runner.java
+++ ie/atu/sw/Runner.java
@@ -1,17 +1,19 @@
 package ie.atu.sw;
 
+/**
+ * This class contains the main method to run the sentiment analysis application.
+ */
 public class Runner {
 
-	public static void main(String[] args) throws Exception {
-		//displayMenu();
-		menu menu = new menu();
-		menu.displayMenu();
-		
-		
-		
-	}
-	
-
-	
-	
+    /**
+     * The main method of the sentiment analysis application.
+     * It creates a new menu and displays it to the user.
+     *
+     * @param args The command-line arguments.
+     * @throws Exception If an error occurs while displaying the menu.
+     */
+    public static void main(String[] args) throws Exception {
+        menu menu = new menu();
+        menu.displayMenu();
+    }
 }
